4faec73e422f99f1076ac89531bfafd0d0d5a702
AryanShinde <you@example.com>
2021-05-17 18:04:06 +0530

    todo-items added

--- app.js
+++ app.js
@@ -7,4 +7,24 @@ button.addEventListener("click",addtodo);
 function addtodo(event)
 {
     event.preventDefault();
+    const tododiv=document.createElement("div");
+    tododiv.classList.add("todo");
+
+    const todoitem=document.createElement("li");
+    todoitem.classList.add("todoitem");
+    todoitem.innerText="item 1";
+    tododiv.appendChild(todoitem);
+    
+    const buttondone=document.createElement("button");
+    const buttontrash=document.createElement("button");
+
+    buttondone.classList.add("button-done");
+    buttontrash.classList.add("button-trash");
+
+    buttondone.innerHTML='<i class="fas fa-check"></i>';
+    tododiv.appendChild(buttondone);
+    buttontrash.innerHTML='<i class="fas fa-trash"></i>';
+    tododiv.appendChild(buttontrash);
+    TodoList.appendChild(tododiv);
+
 }
