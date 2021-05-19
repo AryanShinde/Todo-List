e39fda1cfaad80754ce1228b92d97cb900c21571
AryanShinde <you@example.com>
2021-05-19 21:11:09 +0530

    feat: completed and deleting todos

--- app.js
+++ app.js
@@ -1,34 +1,63 @@
 //selectors
-const input=  document.querySelector(".todo-input");
-const button= document.querySelector(".todo-button");
-const TodoList=document.querySelector(".todo-list")
+const input = document.querySelector(".todo-input");
+const button = document.querySelector(".todo-button");
+const TodoList = document.querySelector(".todo-list")
 
 //eventlisteners
-button.addEventListener("click",addtodo);
+
+button.addEventListener("click", addtodo);
+TodoList.addEventListener("click", deleteitem);
+
 //functions
-function addtodo(event)
-{
+
+function addtodo(event) {
     event.preventDefault();
-    const tododiv=document.createElement("div");
+    const tododiv = document.createElement("div");
     tododiv.classList.add("todo");
 
-    const todoitem=document.createElement("li");
+    const todoitem = document.createElement("li");
     todoitem.classList.add("todoitem");
-    todoitem.innerText=input.value;
+    todoitem.innerText = input.value;
     tododiv.appendChild(todoitem);
-    
-    const buttondone=document.createElement("button");
-    const buttontrash=document.createElement("button");
+
+    const buttondone = document.createElement("button");
+    const buttontrash = document.createElement("button");
 
     buttondone.classList.add("button-done");
     buttontrash.classList.add("button-trash");
 
-    buttondone.innerHTML='<i class="fas fa-check"></i>';
+    buttondone.innerHTML = '<i class="fas fa-check"></i>';
     tododiv.appendChild(buttondone);
-    buttontrash.innerHTML='<i class="fas fa-trash"></i>';
+    buttontrash.innerHTML = '<i class="fas fa-trash"></i>';
     tododiv.appendChild(buttontrash);
     TodoList.appendChild(tododiv);
+    input.value = ""
+}
+//deleting
 
-    input.value=""
+function deleteitem(event) {
 
+    const item = event.target;
+    if (item.classList[0] === "button-trash") {
+        item.parentElement.classList.add("fall");
+
+        //prevents directly delteting the element before transition
+        item.parentElement.addEventListener("transitionend", function()
+        {
+            item.parentElement.remove();
+        });
+
+    }
+
+    //check
+    else if (item.classList[0] === "button-done") {
+
+        item.parentElement.classList.toggle("completed");
+        item.parentElement.addEventListener("transitionend", function()
+        {
+            item.classList.remove("button-done-1");
+        });
+        
+        
+    }
 }
